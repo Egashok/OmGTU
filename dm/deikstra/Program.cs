a697edd3eb0a86dc14fee2fd96dd047119c2cb27
egashok <55044715+Egashok@users.noreply.github.com>
2023-03-11 23:25:18 +0600

    qa й q q
    qqa
    :q
    qaq
    aq
    qa
    :wq

---
+++ dm/deikstra/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] peaks = Console.ReadLine().Split();
+var ribrs = new Dictionary<string, int>();
+string[] inpRibr = Console.ReadLine().Split();
+ribrs.Add(inpRibr[0], Convert.ToInt32(inpRibr[1]));
+string firstPeak = Console.ReadLine();
+string lastPeak = Console.ReadLine();
+List<string> opRibrs = new List<string>();
+List<string> conPeaks = new List<string>();
+conPeaks.Add(firstPeak);
+
+while (!conPeaks.Contains(lastPeak))
+{
+
+
+
+}
