0d87afd8b6237ef9141481258f2a69c691b11b42
egashok <55044715+Egashok@users.noreply.github.com>
2023-03-12 19:42:12 +0600

    finalDeikstr

--- dm/deikstra/main.py
+++ dm/deikstra/main.py
@@ -1,3 +1,5 @@
+#Ввод : ребро1 ребро2 вес
+
 ribrs=[]
 peaks=""
 opPeaks=""
@@ -23,7 +25,7 @@ d = [score]*len(peaks)
 a = int(input('От куда:')) - 1
 b = int(input('Куда:')) - 1
 
-#Вводим точки отправления,прибытия
+#Вводим точки отправления,прибытия и убавляем до индекса
 
 opPeaks = str(a + 1)
 for i in range(len(d)):
@@ -32,13 +34,13 @@ for i in range(len(d)):
             d[i] = ribrs[j][2]
 #находим ребра для первой вершины
 
-minn = score
-minn_ind = None
+min_val = score
+min_peak = None
 for i in range(len(d)):
-    if d[i] < minn and str(i+1) not in opPeaks:
-        minn = d[i]
-        minn_ind = i
-a = minn_ind
+    if d[i] < min_val and str(i+1) not in opPeaks:
+        min_val = d[i]
+        min_peak = i
+a = min_peak
 #ищем минимальное для первой вершины
 while a != None:
     opPeaks += str(a + 1)
@@ -46,13 +48,13 @@ while a != None:
         for j in range(len(ribrs)):
             if ribrs[j][0] == str(a + 1) and ribrs[j][1] == str(i+1):
                 d[i] = min(ribrs[j][2] + d[a], d[i])
-    minn = score
-    minn_ind = None
+    min_val = score
+    min_peak = None
     for i in range(len(d)):
-        if d[i] < minn and str(i+1) not in opPeaks:
-            minn = d[i]
-            minn_ind = i
-    a = minn_ind
+        if d[i] < min_val and str(i+1) not in opPeaks:
+            min_val = d[i]
+            min_peak = i
+    a = min_peak
 #ищем минимальные для остальных
 #d[b]-вес конечной вершины
 print(d[b])
