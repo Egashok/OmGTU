cd23094bdaa685049ef0f0f63157b961e13c7add
egashok <55044715+Egashok@users.noreply.github.com>
2023-05-05 11:48:45 +0600

    as

--- labClass/lab6/Program.cs
+++ labClass/lab6/Program.cs
@@ -1,25 +1,54 @@
 ﻿
-Console.WriteLine("Сколько объектов");
-int objCount = Convert.ToInt32(Console.ReadLine());
-MainClass<int> main = new MainClass<int>(3);
-foreach (var i in Enumerable.Range(0, objCount))
-{
+MainClass<int> intArray = new MainClass<int>(0);
+while (true){
+    Console.Clear();
+    Console.WriteLine("1.Добавить элемент");
+    Console.WriteLine("2.Удалить элемент");
+    Console.WriteLine("3.Получить по индексу");
+    Console.WriteLine("Space.Вывод элементов");
+    Console.WriteLine("4.Выход");
+    ConsoleKeyInfo key;
+    key=Console.ReadKey();
+    Console.Clear();
+    if(key.Key==ConsoleKey.D1){
+        Console.WriteLine("Введите объект:");
+        intArray.Add(Convert.ToInt32(Console.ReadLine()));
+
+    }
+    if(key.Key==ConsoleKey.D2){
+        Console.WriteLine("Введите индекс:");
+        intArray.Delete(Convert.ToInt32(Console.ReadLine()));
+
+    }
+    if(key.Key==ConsoleKey.D3){
+        Console.WriteLine("Введите индекс:");
+        Console.WriteLine(intArray.Get(Convert.ToInt32(Console.ReadLine())));
+
+    }
+    if(key.Key==ConsoleKey.Spacebar){
+        foreach(var i in intArray.arr){
+            Console.WriteLine(i);
+        }
+        Console.ReadKey();
+    }
+    if(key.Key==ConsoleKey.D4){
+        break;
+
+    }
+
+
 }
+
+
 
 
 public class MainClass<T>
 {
     public T[] arr;
-    public MainClass(int size)
-    {
-        arr = new T[size];
+    public MainClass(int size){
+        arr=new T[size];
     }
-
-    public void Set(int index, T value)
-    {
-        arr[index] = value;
-    }
-
+  
     public T Get(int index)
     {
         return arr[index];
---
+++ labClass/lab7/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Garage{
+    List<Car> carList=new List<Car>();
+}
+public class Car{
+    int number;
+}
+public class Wash{
+    
+}
