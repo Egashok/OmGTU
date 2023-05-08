5a7d2454ac795648acb6bd3a5cf06d513d01e9f0
egashok <55044715+Egashok@users.noreply.github.com>
2023-05-08 10:13:51 +0600

    cppp

---
+++ labClass/lab8/Program.cs
@@ -0,0 +1,30 @@
+﻿class Calculator
+{
+    public delegate void Operation(double a, double b);
+
+    public event Operation AddEvent;
+    public event Operation SubtractEvent ;
+    public event Operation MultiplyEvent;
+    public event Operation DivideEvent;
+
+     public void Add(double a, double b)
+    {
+        Console.WriteLine($"{a} + {b} = {a + b}");
+        AddEvent?.Invoke(a, b);
+    }
+     public void Substract(double a, double b)
+    {
+        Console.WriteLine($"{a} - {b} = {a - b}");
+        SubtractEvent?.Invoke(a, b);
+    }
+     public void Multiply(double a, double b)
+    {
+        Console.WriteLine($"{a} + {b} = {a + b}");
+        MultiplyEvent?.Invoke(a, b);
+    }
+     public void Devide(double a, double b)
+    {
+        Console.WriteLine($"{a} + {b} = {a + b}");
+        DivideEvent?.Invoke(a, b);
+    }
+}
