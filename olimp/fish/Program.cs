ca05482fe0eaa500b67853bd9e79a104111fd6a6
egashok <55044715+Egashok@users.noreply.github.com>
2023-05-12 16:05:25 +0600

    fish

--- olimp/fish/Program.cs
+++ olimp/fish/Program.cs
@@ -1,29 +1,4 @@
-﻿/*List<string> words = new List<string>();
-List<string> wordStart = new List<string>();
-List<string> wordEnd = new List<string>();
-StreamReader f = new StreamReader("input.txt");
-
-int n = Convert.ToInt32(f.ReadLine());
-while (n != 0)
-{
-    words.Add(f.ReadLine());
-    n--;
-}
-int k = Convert.ToInt32(f.ReadLine());
-while (k != 0)
-{
-    wordStart.Add(f.ReadLine());
-    k--;
-}
-int l = Convert.ToInt32(f.ReadLine());
-while (l != 0)
-{
-    wordEnd.Add(f.ReadLine());
-    l--;
-}
-f.Close();
-*/
-string[] f = File.ReadAllLines(@"D:\Золотая рыбка\input_s1_12.txt");
+﻿string[] f = File.ReadAllLines("input.txt");
 int n = int.Parse(f[0]);
 string[] words = new string[n];
 
@@ -33,73 +8,68 @@ for (int i = 0; i < n; i++)
 }
 
 Dictionary<string, int> FirstChar = new Dictionary<string, int>();
-int n1 = int.Parse(f[n + 1]);
+int n1 = Convert.ToInt32((f[n + 1]));
 for (int i = n + 2; i < n + 2 + n1; i++)
 {
     string[] str = f[i].Split();
-    FirstChar.Add(str[0], int.Parse(str[1]));
+    FirstChar.Add(str[0], Convert.ToInt32((str[1])));
 }
 
 Dictionary<string, int> LastChar = new Dictionary<string, int>();
-int n2 = int.Parse(f[n + n1 + 2]);
+int n2 = Convert.ToInt32((f[n + n1 + 2]));
 for (int i = n + n1 + 3; i < n + n2 + 3 + n1; i++)
 {
     string[] str = f[i].Split();
-    LastChar.Add(str[0], int.Parse(str[1]));
+    LastChar.Add(str[0], Convert.ToInt32((str[1])));
 }
 
-Dictionary<string, int> combinations = new Dictionary<string, int>();
+Dictionary<string, int> comb = new Dictionary<string, int>();
 foreach (string word in words)
 {
     if (FirstChar.ContainsKey((Convert.ToString(word[0]))) && LastChar.ContainsKey((Convert.ToString(word[word.Length - 1]))))
     {
         string key = Convert.ToString(word[0]) + Convert.ToString(word[word.Length - 1]);
-        if (combinations.ContainsKey(key))
+        if (comb.ContainsKey(key))
         {
-            combinations[key]++;
+            comb[key]++;
         }
         else
         {
-            combinations.Add(key, 1);
+            comb.Add(key, 1);
         }
     }
 }
 
 int Count = 0;
 bool Flag = true;
-while (combinations.Count > 0 && Flag)
+while (comb.Count > 0 && Flag)
 {
     Flag = false;
-    Console.WriteLine("------------------");
-    foreach (var combination in combinations)
+    foreach (var combination in comb)
     {
         string key = combination.Key;
         Console.Write($"key: {key}");
         int minn = Math.Min(FirstChar[Convert.ToString(key[0])], LastChar[Convert.ToString(key[1])]);
-        Console.WriteLine($" value: {combinations[key]}  {key[0]}:{FirstChar[Convert.ToString(key[0])]} {key[1]}:{LastChar[Convert.ToString(key[1])]} {Count}");
         if (combination.Value < minn)
         {
             Count++;
             FirstChar[Convert.ToString(key[0])]--;
             LastChar[Convert.ToString(key[1])]--;
-            combinations[key]--;
+            comb[key]--;
         }
-        if (combinations[key] == 0 || FirstChar[Convert.ToString(key[0])] == 0 || LastChar[Convert.ToString(key[1])] == 0)
+        if (comb[key] == 0 || FirstChar[Convert.ToString(key[0])] == 0 || LastChar[Convert.ToString(key[1])] == 0)
         {
-            combinations.Remove(key);
+            comb.Remove(key);
         }
-        if (combinations.ContainsKey(key) && combinations[key] != combination.Value)
+        if (comb.ContainsKey(key) && comb[key] != combination.Value)
         {
             Flag = true;
         }
     }
 }
 
-Console.WriteLine(Count);
-Console.WriteLine(combinations.Count);
-while (combinations.Count > 0)
+while (comb.Count > 0)
 {
-    //string key = combinations.FirstOrDefault(x => x.Value == combinations.Values.Max()).Key;
     string key = "";
     int key_value = 0;
     foreach (var first in FirstChar)
@@ -110,7 +80,7 @@ while (combinations.Count > 0)
             string key_last = last.Key;
             string all_key = key_first + key_last;
             int minn = Math.Min(first.Value,last.Value);
-            if (minn != 0 && minn > key_value && combinations.ContainsKey(all_key))
+            if (minn != 0 && minn > key_value && comb.ContainsKey(all_key))
             {
                 key = all_key;
                 key_value = minn;
@@ -119,38 +89,37 @@ while (combinations.Count > 0)
     }
     if(key == "")
     {
-        key = key = combinations.FirstOrDefault(x => x.Value == combinations.Values.Max()).Key;
+        key = key = comb.FirstOrDefault(x => x.Value == comb.Values.Max()).Key;
     }
-    if (combinations[key] > 0 && FirstChar[Convert.ToString(key[0])] > 0 && LastChar[Convert.ToString(key[1])] > 0)
+    if (comb[key] > 0 && FirstChar[Convert.ToString(key[0])] > 0 && LastChar[Convert.ToString(key[1])] > 0)
     {
         Count++;
         FirstChar[Convert.ToString(key[0])]--;
         LastChar[Convert.ToString(key[1])]--;
-        combinations[key]--;
+        comb[key]--;
     }
-    if (combinations[key] == 0 || FirstChar[Convert.ToString(key[0])] == 0 || LastChar[Convert.ToString(key[1])] == 0)
+    if (comb[key] == 0 || FirstChar[Convert.ToString(key[0])] == 0 || LastChar[Convert.ToString(key[1])] == 0)
     {
-        combinations.Remove(key);
+        comb.Remove(key);
     }
 }
 
-Console.WriteLine(Count);
-Console.WriteLine(combinations.Count);
-while(combinations.Count > 0)
+
+while(comb.Count > 0)
 {
-    foreach (var combination in combinations)
+    foreach (var combination in comb)
     {
         string key = combination.Key;
-        if (combinations[key] > 0 && FirstChar[Convert.ToString(key[0])] > 0 && LastChar[Convert.ToString(key[1])] > 0)
+        if (comb[key] > 0 && FirstChar[Convert.ToString(key[0])] > 0 && LastChar[Convert.ToString(key[1])] > 0)
         {
             Count++;
             FirstChar[Convert.ToString(key[0])]--;
             LastChar[Convert.ToString(key[1])]--;
-            combinations[key]--;
+            comb[key]--;
         }
-        if (combinations[key] == 0 || FirstChar[Convert.ToString(key[0])] == 0 || LastChar[Convert.ToString(key[1])] == 0)
+        if (comb[key] == 0 || FirstChar[Convert.ToString(key[0])] == 0 || LastChar[Convert.ToString(key[1])] == 0)
         {
-            combinations.Remove(key);
+            comb.Remove(key);
         }
     }
 }
