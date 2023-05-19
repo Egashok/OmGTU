8d8bfab7e8a8fd0ce1b38d9a037239801ae90303
egashok <55044715+Egashok@users.noreply.github.com>
2023-05-19 15:22:55 +0600

    rzg

---
+++ dm/compoment/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> ribrs=new List<string>();
+Console.WriteLine("Введите рёбра:");//кроме нуля
+while(true){
+    string ribr=Console.ReadLine();
+    if(ribr!="0"){
+    ribrs.Add(ribr);
+    }
+    else{
+        break;
+    }
+}
